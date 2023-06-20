2bda4898e9712ee24dfebdd076b1d63155f7ecea
kangash <kangash.aleksei@gmail.com>
2023-06-20 10:02:52 +0300

    added task 5

--- course_practice.js
+++ course_practice.js
@@ -40,3 +40,9 @@ function testWhile(a) {
   }
   return x;
 }
+
+/* Задание 5. Дано натуральное число n. Напишите рекурсивную функцию, которая возвращает строку чисел от 1 до n, разделенных пробелом. */
+function my_function(n) {
+  if (n == 1) return n;
+  return my_function(n - 1) + ` ${n}`;
+}
