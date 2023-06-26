dee1cb6ddc45d89b33f00e7e0df9ba2a52ff3830
kangash <kangash.aleksei@gmail.com>
2023-06-26 16:08:39 +0300

    add task 3

--- js_trainer.js
+++ js_trainer.js
@@ -1,5 +1,5 @@
 /* 1. Напишите код, который определяет является ли число четным или нечетным. */
-function checkNumber(num) {
+function checkParity(num) {
   let result = num % 2 == 0 ? "четное" : "нечетное";
   console.log(result);
 }
@@ -9,3 +9,6 @@ function compareNumbers(x, y) {
   let result = x > y ? "x больше y" : x < y ? "y больше x" : "x равно y";
   console.log(result);
 }
+
+/* 3. Напишите код, который в зависимости от значения переменной role записывает соответствующий результат в переменную result.
+1 — admin, 2 — moderator, 3 — user */
